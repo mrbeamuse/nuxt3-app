c8e2999d08252ff56d7e3f173ab72b8015308e3f
beams <834483878@qq.com>
2023-09-07 10:52:05 +0800

    组件自动导入和naiveUI的SSR导入

---
+++ components/Avatar.vue
@@ -0,0 +1,10 @@
+<template>
+  <img
+    class="w-[50px] border-[1px] border-slate-300 rounded-full inline-block"
+    src="~/assets/avatar.png"
+    alt="avatar"
+  />
+</template>
+<script setup></script>
+
+<style lang="scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>Index Page</div>
   <a href="">超链接</a>
+  <div>
+    <NButton>hello</NButton>
+  </div>
   <!--跳转链接-->
   <NuxtLink to="/detail/1">Detail Page</NuxtLink>
 </template>

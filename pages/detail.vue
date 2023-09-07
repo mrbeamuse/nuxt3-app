1ea7779eb94988a576519630dac8b0e81fa36b8e
beams <834483878@qq.com>
2023-09-07 09:43:30 +0800

    nuxt初始化及文件路由和布局了解

---
+++ pages/detail.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Detail Page</h1>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Index Page</div>
+  <!--跳转链接-->
+  <NuxtLink to="/detail/1">Detail Page</NuxtLink>
+</template>
